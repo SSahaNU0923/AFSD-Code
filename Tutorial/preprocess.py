7bf876491952093b7238616a8d1b9b88815a53c1
Sourav Saha <68312260+SSahaNU0923@users.noreply.github.com>
2025-10-22 07:45:28 -0400

    Update preprocess.py

--- Tutorial/preprocess.py
+++ Tutorial/preprocess.py
@@ -1,3 +1,12 @@
+#------------------------------------------------------------------------------
+# This file is part of AFSD-Code, adapted from:
+# AM_Thermomechancial_Solver by Shuheng Liao
+# https://github.com/ShuhengLiao/AM_Thermomechanical_Solver
+# Orignial Cppyright (c) 2023 Shuheng Liao
+# Modificiations Copyright (c) 2025 Sourav Saha
+# Licensed under the MIT License; see LICENSE file for details
+#------------------------------------------------------------------------------
+
 from preprocessor import write_keywords,write_birth,write_parameters
 
 import time
@@ -13,4 +22,5 @@ radius = 15
 path_resolution = 0.75 # half of the element size
 write_keywords(file_name,output_file,substrate_height)
 write_birth(output_file,toolpath_file,path_resolution,radius,gif_end=5,nFrame=50,mode=1,camera_position=[(0, -50, 75),(0, 0, 0),(0.0, 0.0, 1.0)])
+
 write_parameters(output_file)
